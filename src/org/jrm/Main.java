3e6f5b3e74a06806c5107837fcd5369b6870d99a
Jared R. Mallas <jrtytn@gmail.com>
2018-09-05 20:22:55 -0500

    need to calc averages

--- src/org/jrm/Main.java
+++ src/org/jrm/Main.java
@@ -2,10 +2,13 @@ package org.jrm;
 
 import java.util.Scanner;
 
+// TODO: calculate average rating derived from aggregating movie_rating.csv
 public class Main {
 
     private final static FileInput cardAccts = new FileInput("movie_cards.csv");
     private final static FileInput cardPurchases = new FileInput("movie_purchases.csv");
+    private final static FileInput cardRatings = new FileInput("movie_rating.csv");
+
     private static Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -37,5 +40,38 @@ public class Main {
             }
 
         }
+    }
+
+    public static void findAverageRating(String acct, int[] nums) {
+        nums[0] = 0;
+        nums[1] = 0;
+        String line;
+        String[] fields;
+        boolean done = false;
+        while((line = cardRatings.fileReadLine()) != null)
+        {
+            fields = line.split(",");
+            if (fields[0].compareTo(acct) > 0)
+            {
+                // If the first field of the card rating file doesn't match the acct
+                // break out after calculating average rating...
+            }
+            else if ()
+            {
+                // If the first field of the card rating matches acct
+                // add to total rating
+            }
+        }
+        /* while (((line = cardPurchases.fileReadLine()) != null) && !(done)) {
+            fields = line.split(",");
+            if (fields[0].compareTo(acct) > 0) {
+                done = true;
+            }
+            else if (fields[0].equals(acct)) {
+                nums[0]++;
+                nums[1] += Integer.parseInt(fields[2]);
+            }
+
+        }*/
     }
 }
